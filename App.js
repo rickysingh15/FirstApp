b57068c7df1dc00756f45f41ca2b77e425bb951b
Tushar Singh <rickysingh15.rs@gmail.com>
2024-10-06 18:35:12 +0530

    feature/1 #first commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
 
-export default function App() {
+export default function App() 
+{
+
+  const [textInputString, textInputHandler] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(receivedText) {
+    // console.log(receivedText);
+    textInputHandler(receivedText);
+  }
+
+  function addGoalHandler() {
+    console.log(textInputString);
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, textInputString]);   
+    
+    textInputHandler('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+
+      <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler}/>
+          <Button title="Add Goal" onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    padding: 8,
+    marginRight: 8,
+    width: '70%'
+  },
+  goalsContainer: {
+    marginTop: 16,
+    paddingTop: 16,
+    flex: 5
+  }
 });
